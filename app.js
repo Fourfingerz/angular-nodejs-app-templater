d75b40518f7e3e3ad5d34063254c61068a0d9f1a
Fourfingerz <chansd5@gmail.com>
2020-03-29 21:02:20 +0000

    Got minimum expressjs server setup working with Hello World

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-
 var app = express();
 
 // view engine setup
@@ -17,9 +15,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'client/src')));
 
-app.use('/', indexRouter);
+app.use('/', require('./routes/index'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello Angular App!' });
+  // res.render('index', { title: 'Hello Angular App!' });
+  res.send('<h1>HELLO ANGULAR</h1>');
 });
-
 module.exports = router;
