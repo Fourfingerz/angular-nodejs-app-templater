30c175e57b3ec94d9b2c01d7903d059a1583df5f
Fourfingerz <chansd5@gmail.com>
2020-04-12 15:55:15 -0400

    Got a working express server

---
+++ server/server.js
@@ -0,0 +1,24 @@
+"use strict";
+exports.__esModule = true;
+// equivalent of older: const express = require('express')
+var express = require("express");
+var app = express(); // Allow any method from any host and log requests
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+    if ('OPTIONS' === req.method) {
+        res.sendStatus(200);
+    }
+    else {
+        console.log(req.ip + " " + req.method + " " + req.url);
+        next();
+    }
+}); // Handle POST requests that come in formatted as JSON
+app.use(express.json()); // A default hello word route
+app.get('/', function (req, res) {
+    res.send({ hello: 'world' });
+}); // start our server on port 4201
+app.listen(4201, '127.0.0.1', function () {
+    console.log("Server now listening on 4201");
+});
